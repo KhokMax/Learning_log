b970f4657e93ec73e91dea7e5909d6825c411d54
KhokMaksim <khok.max.2002@gmail.com>
2022-06-28 19:07:46 +0300

    Ready for deployment to heroku.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,7 +2,6 @@ from ast import mod
 from cgitb import text
 import re
 from tabnanny import verbose
-from tkinter import E
 from django.db import models
 from django.contrib.auth.models import User
 
